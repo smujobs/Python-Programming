6d2c38ddda595a444c17de72d82ca64504499905
smujobs <wondev112@gmail.com>
2023-04-14 14:10:34 +0900

    hw6 upload

---
+++ test/test3.py
@@ -0,0 +1,12 @@
+def disp_multi_table(num):
+    for sub in range(1, 10):
+        print(f'{num} x {sub} = {str(num * sub).rjust(2)}')
+        
+
+def input_pos_num():
+    n = 0
+    while n <= 0:
+        n = int(input('구구단에 들어갈 양의 정수를 입력하세요: '))
+    return n
+
+disp_multi_table(input_pos_num())
---
+++ upload/hw6.py
@@ -0,0 +1,18 @@
+"""
+202310977 정지원
+2~9단의 구구단을 출력하는 프로그램
+"""
+
+def display_line_product_set(start, end):
+    for sub_num in range(1, 10):
+        for main_num in range(start, end + 1):
+            product = main_num * sub_num
+            product_str = str(product).rjust(2)
+            print(f'{main_num} x {sub_num} = {product_str}', end='     ')
+        print()
+
+# main
+
+display_line_product_set(2, 5)
+print()
+display_line_product_set(6, 9)
